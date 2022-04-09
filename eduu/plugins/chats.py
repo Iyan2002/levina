83b3483e2053f1c327ffe5a4b364517ce05a6873
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-09 16:08:58 +0700

    [ src ] project started

--- eduu/plugins/chats.py
+++ eduu/plugins/chats.py
@@ -1,13 +1,7 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2022 Amano Team
-
 from pyrogram import Client
 from pyrogram.types import Message
 
 from eduu.utils import add_chat, chat_exists
-
-# This is the first plugin run to guarantee
-# that the actual chat is initialized in the DB.
 
 
 @Client.on_message(group=-1)
