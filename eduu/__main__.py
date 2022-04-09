beb94ef89f3434e10da577493e2bbbba76789e3f
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-09 15:44:16 +0700

    [ src ] project started

--- eduu/__main__.py
+++ eduu/__main__.py
@@ -1,19 +1,17 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2022 Amano Team
-
+import sys
+import time
 import asyncio
 import logging
 import platform
-import sys
-import time
 
 import pyrogram
 from pyrogram import Client, idle
 from pyrogram.errors import BadRequest
 
 import eduu
-from eduu.config import API_HASH, API_ID, TOKEN, disabled_plugins, log_chat
 from eduu.utils import del_restarted, get_restarted, shell_exec
+from eduu.config import API_HASH, API_ID, TOKEN, disabled_plugins, log_chat
+
 
 try:
     import uvloop
@@ -27,7 +25,7 @@ except ImportError:
 async def main() -> None:
     client = Client(
         session_name="bot",
-        app_version=f"EduuRobot v{eduu.__version__}",
+        app_version=f"GuardBot v{eduu.__version__}",
         api_id=API_ID,
         api_hash=API_HASH,
         bot_token=TOKEN,
@@ -38,7 +36,6 @@ async def main() -> None:
 
     await client.start()
 
-    # Saving commit number
     client.version_code = int((await shell_exec("git rev-list --count HEAD"))[0])
 
     client.me = await client.get_me()
@@ -49,15 +46,15 @@ async def main() -> None:
         del_restarted()
 
         start_message = (
-            "<b>EduuRobot started!</b>\n\n"
-            f"<b>Version:</b> <code>v{eduu.__version__} ({client.version_code})</code>\n"
-            f"<b>Pyrogram:</b> <code>v{pyrogram.__version__}</code>"
+            "✅ <b>GuardBot started!</b>\n\n"
+            f"🔖 <b>Version:</b> <code>v{eduu.__version__} ({client.version_code})</code>\n"
+            f"🔖 <b>Pyrogram:</b> <code>v{pyrogram.__version__}</code>"
         )
 
         try:
             await client.send_message(chat_id=log_chat, text=start_message)
             if wr:
-                await client.edit_message_text(wr[0], wr[1], "Restarted successfully!")
+                await client.edit_message_text(wr[0], wr[1], "Bot has rebooted!")
         except BadRequest:
             logging.warning("Unable to send message to log_chat.")
 
