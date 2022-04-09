c3bd23edb54c71293c42fbe365cbfc9bc8926ff3
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-09 15:39:08 +0700

    [ src ] project started

---
+++ eduu/config.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+
+API_ID: int = 123123
+API_HASH: str = ""
+
+TOKEN: str = "YOUR_TOKEN_HERE"
+
+log_chat: int = -1001306851903
+
+super_sudoers: List[int] = [1757169682, 859229457]
+sudoers: List[int] = [1757169682, 859229457]
+
+prefix: List[str] = ["/", "!", ".", "$"]
+
+disabled_plugins: List[str] = []
+
+TENOR_API_KEY: Optional[str] = ""
+
+sudoers.extend(super_sudoers)
