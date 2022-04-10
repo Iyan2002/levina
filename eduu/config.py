183d4271a60fb235da35f9f30a55f2e4fb486ea0
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-10 20:31:31 +0700

    [ setup ] input tenor api_key

--- eduu/config.py
+++ eduu/config.py
@@ -11,10 +11,10 @@ log_chat: int = -1001306851903
 super_sudoers: List[int] = [1757169682, 859229457]
 sudoers: List[int] = [1757169682, 859229457]
 
-prefix: List[str] = ["/", "!", ".", "$"]
+prefix: List[str] = ["/", "!", ".", "$", "-"]
 
 disabled_plugins: List[str] = []
 
-TENOR_API_KEY: Optional[str] = ""
+TENOR_API_KEY: Optional[str] = "X9HD35B7ZGP6"
 
 sudoers.extend(super_sudoers)
