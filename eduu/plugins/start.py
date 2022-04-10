aa2b21f8dc5dfb63c1ae161fcd455eff98668493
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-10 22:15:01 +0700

    [ fix ] int value error

--- eduu/plugins/start.py
+++ eduu/plugins/start.py
@@ -49,7 +49,7 @@ async def start(c: Client, m: Union[Message, CallbackQuery], strings):
                 ],
             ]
         )
-        await method(strings("private"), reply_markup=keyboard)
+        await method(strings("private"), reply_markup=keyboard, disable_web_page_preview=True)
     else:
         keyboard = InlineKeyboardMarkup(
             inline_keyboard=[
@@ -61,7 +61,7 @@ async def start(c: Client, m: Union[Message, CallbackQuery], strings):
                 ]
             ]
         )
-        await method(strings("group"), reply_markup=keyboard)
+        await method(strings("group"), reply_markup=keyboard, disable_web_page_preview=True)
 
 
 @Client.on_callback_query(filters.regex("^infos$"))
