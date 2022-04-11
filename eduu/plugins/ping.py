9a636a73533a73da02bf1072027779eb2040e18c
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-11 07:23:42 +0700

    [ txt ] just a little change

--- eduu/plugins/ping.py
+++ eduu/plugins/ping.py
@@ -10,7 +10,7 @@ from eduu.utils import commands
 @Client.on_message(filters.command("ping", prefix))
 async def ping(c: Client, m: Message):
     first = datetime.now()
-    sent = await m.reply_text("<b>Pong!</b>")
+    sent = await m.reply_text("<b>pinging...</b>")
     second = datetime.now()
     await sent.edit_text(
        f"🏓 <b>PONG !</b>\n⏱ <code>{(second - first).microseconds / 1000}</code> ms"
