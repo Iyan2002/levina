e92e760a14979c06dfee67d206ad53f55314721a
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-12 22:23:23 +0700

    [ src ] project started

--- eduu/plugins/chats.py
+++ eduu/plugins/chats.py
@@ -1,12 +1,6 @@
-from pyrogram import Client, filters
-from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-)
+from pyrogram import Client
 
 from eduu.utils import add_chat, chat_exists
-from eduu.utils.localization import use_chat_lang
 
 
 @Client.on_message(group=-1)
@@ -17,46 +11,3 @@ async def check_chat(c: Client, m: Message):
     # task: run function to add chat into database
     if not chat_exists(chat_id, chat_type):
         add_chat(chat_id, chat_type)
-
-
-@Client.on_message(filters.new_chat_members)
-@use_chat_lang()
-async def new_chat(c: Client, m: Message, strings):
-    chat_id = m.chat.id
-
-    for new in m.new_chat_members:
-        keyboard_1 = InlineKeyboardMarkup(
-            inline_keyboard = [
-                [
-                    InlineKeyboardButton(
-                        strings("channel_button_txt"), url="https://t.me/levinachannel",
-                    ),
-                    InlineKeyboardButton(
-                        strings("support_button_txt"), url="https://t.me/VeezSupportGroup",
-                    )
-                ],
-            ]
-        )
-        keyboard_2 = InlineKeyboardMarkup(
-            inline_keyboard = [
-                [
-                    InlineKeyboardButton(
-                        strings("language_button_txt"), callback_data="chlang",
-                    ),
-                    InlineKeyboardButton(
-                        strings("commands_button_txt"), url="https://t.me/GroupsGuardRobot?start=help",
-                    )
-                ],
-            ]
-        )
-        
-        try:
-            if new.id == c.id:
-                return await m.reply_text(
-                    strings("greetings_add_chat"), reply_markup=keyboard_1
-                )
-            return await m.reply_text(
-                strings("introducing_usages"), reply_markup=keyboard_2
-            )
-        except Exception:
-            return
