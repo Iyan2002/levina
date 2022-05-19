da88d90863871e9d8f0cfef42c5b39f1e637217f
levina <82658782+levina-lab@users.noreply.github.com>
2022-05-19 15:14:31 +0700

    [ config ] additional changes

--- eduu/config.py
+++ eduu/config.py
@@ -1,19 +1,23 @@
+import os.path
+
 from typing import List, Optional
 
 
 API_ID: int = 3487995
 API_HASH: str = "7b9f1868c1e90b7408d48445f1e89603"
-
 TOKEN: str = "5200427414:AAHo-4LnDtqC6rzAv2lCuwqWTGBkilxIKFU"
 
 log_chat: int = -1001306851903
-
-super_sudoers: List[int] = [1757169682, 859229457]
 sudoers: List[int] = [1757169682, 859229457]
+super_sudoers: List[int] = [1757169682, 859229457]
 
 prefix: List[str] = ["/", "!", ".", "$", "-"]
 
 disabled_plugins: List[str] = []
+
+WORKERS = 24
+
+DATABASE_PATH = os.path.join("eduu", "database", "eduu.db")
 
 TENOR_API_KEY: Optional[str] = "X9HD35B7ZGP6"
 
