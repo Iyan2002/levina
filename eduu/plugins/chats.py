9fba05268c38aa1dd4c5e179fe1de4345fc50595
levina <82658782+levina-lab@users.noreply.github.com>
2022-05-19 16:33:11 +0700

    [ initial ] major changes update

--- eduu/plugins/chats.py
+++ eduu/plugins/chats.py
@@ -1,13 +1,14 @@
 from pyrogram import Client
 from pyrogram.types import Message
 
-from eduu.utils import add_chat, chat_exists
+from eduu.database.chats import add_chat, chat_exists
 
 
 @Client.on_message(group=-1)
 async def check_chat(c: Client, m: Message):
     chat_id = m.chat.id
     chat_type = m.chat.type
+    chatexists = await chat_exists(chat_id, chat_type)
 
-    if not chat_exists(chat_id, chat_type):
-        add_chat(chat_id, chat_type)
+    if not chatexists(chat_id, chat_type):
+        await add_chat(chat_id, chat_type)
