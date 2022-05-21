6b2dee51a2c07abc8fc18540c9951d5640e3f684
levina <82658782+levina-lab@users.noreply.github.com>
2022-05-21 21:04:02 +0700

    [ client ] telegram bot token expired

--- eduu/config.py
+++ eduu/config.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 
 API_ID: int = 3487995
 API_HASH: str = "7b9f1868c1e90b7408d48445f1e89603"
-TOKEN: str = "5200427414:AAHo-4LnDtqC6rzAv2lCuwqWTGBkilxIKFU"
+TOKEN: str = "5200427414:AAFaGBEsZZ1vApatFyThW_gx8diVCWMLwbA"
 
 log_chat: int = -1001306851903
 sudoers: List[int] = [1757169682, 859229457]
