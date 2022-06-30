1c2e6ceba00fcf498c6566d98ea5f9d9bdc7652e
levina <82658782+levina-lab@users.noreply.github.com>
2022-06-30 14:20:20 +0700

    [ fix ] telegraph & batbin paste issues

--- eduu/plugins/nekobin.py
+++ eduu/plugins/nekobin.py
@@ -1,28 +1,80 @@
+import os
+import re
+import aiofiles
+import aiohttp.ClientSession
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from eduu.config import prefix
-from eduu.utils import commands, http
+from eduu.utils import commands
 from eduu.utils.localization import use_chat_lang
+
+
+aiohttpsession = ClientSession()
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttpsession.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+BASE = "https://batbin.me/"
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
 
 
 @Client.on_message(filters.command("paste", prefix))
 @use_chat_lang(context="pastes")
 async def nekobin(c: Client, m: Message, strings):
-    if m.reply_to_message:
-        if m.reply_to_message.document:
-            tfile = m.reply_to_message
-            to_file = await tfile.download()
-            with open(to_file, "rb") as fd:
-                mean = fd.read().decode("UTF-8")
-        if m.reply_to_message.text:
-            mean = m.reply_to_message.text
-        url = "https://nekobin.com/api/documents"
-        r = await http.post(url, json={"content": mean})
-        url = f"https://nekobin.com/{r.json()['result']['key']}"
-        await m.reply_text(url, disable_web_page_preview=True)
-    else:
-        await m.reply_text(strings("reply_to_document_or_text"))
+    if not m.reply_to_message:
+        return await m.reply_text(strings("reply_to_document_or_text"))
+
+    r = m.reply_to_message
+
+    if not r.text and not r.document:
+        return await m.reply_text(
+            strings("supported_text_document")
+        )
+
+    msg = await m.reply_text(strings("paste_upload_process"))
+
+    if r.text:
+        content = str(r.text)
+    elif r.document:
+        if r.document.file_size > 40000:
+            return await msg.edit_text(strings("paste_file_limit"))
+        if not pattern.search(r.document.mime_type):
+            return await msg.edit_text(strings("paste_text_only"))
+
+        doc = await m.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+
+        os.remove(doc)
+
+    url = await paste(content)
+    try:
+        if m.from_user.is_bot:
+            await m.reply_photo(
+                photo=url,
+                quote=False,
+            )
+        else:
+            await m.reply_photo(
+                photo=url,
+                quote=False,
+                caption=f"🔗 {link}",
+            )
+        await msg.delete()
+    except Exception:
+        await msg.edit_text(f"🔗 {link}")
 
 
 commands.add_command("paste", "tools", "nekobin_description", context_location="pastes")
--- eduu/plugins/telegraph.py
+++ eduu/plugins/telegraph.py
@@ -1,9 +1,15 @@
+import telegraph.Telegraph
+
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
 from eduu.config import prefix
 from eduu.utils import commands, http
 from eduu.utils.localization import use_chat_lang
+
+
+telegraph = Telegraph()
+telegraph.create_account(short_name="GroupsGuardRobot")
 
 
 @Client.on_message(filters.command(["telegraph", "tgm"], prefix))
@@ -23,8 +29,21 @@ async def telegraph(c: Client, m: Message, strings):
                 "https://telegra.ph/upload", files={"upload-file": open(tg_file, "rb")}
             )
             tele_link = "https://telegra.ph" + response.json()[0]["src"]
-            await msg.edit_text(
+            return await msg.edit_text(
                 strings("telegraph_upload_done").format(url=tele_link)
+            )
+        if m.reply_to_message.text:
+            if len(m.command) < 2:
+                return await m.reply_text(strings("telegraph_text_example"))
+            msg = await c.send_message(
+                m.chat.id, strings("telegraph_upload_load")
+            )
+            name = m.text.split(None, 1)[1]
+            page = telegraph.create_page(
+                name, html_content=m.reply_to_message.text.html.replace("\n", "<br>")
+            )
+            return await msg.edit_text(
+                strings("telegraph_upload_done").format(url={page['url']})
             )
     else:
         await m.reply_text(strings("telegraph_err_no_reply"))
