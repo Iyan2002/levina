f33e65aa1a18fc37f559828761ad870814188a1b
Reynaldi Siregar <levinashavila@gmail.com>
2022-10-10 12:41:08 +0700

    [ fix ] remove an unused thing

--- eduu/__init__.py
+++ eduu/__init__.py
@@ -1,13 +1,11 @@
 """GuardBot core package"""
 
 from subprocess import run
-from telegraph import Telegraph
 
-__version__ = "1.0.2"
+__version__ = "2.1.0"
 __version_code__ = (
-    run(["git", "rev-list", "--count", "HEAD"],
-    capture_output=True).stdout.decode().strip() or "0"
+    run(["git", "rev-list", "--count", "HEAD"], capture_output=True)
+    .stdout.decode()
+    .strip()
+    or "0"
 )
-
-telegraph = Telegraph()
-telegraph.create_account(short_name="GroupsGuardRobot")
