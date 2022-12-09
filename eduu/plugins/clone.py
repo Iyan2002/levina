0b494df8875099843f2fefcb314a1d0d36da20fc
Reynaldi Siregar <levinashavila@gmail.com>
2022-12-09 16:54:33 +0700

    [ new ] add clone feature (beta test)

---
+++ eduu/plugins/clone.py
@@ -0,0 +1,33 @@
+import re
+
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message
+from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+
+from ..config import API_ID, API_HASH
+
+
+@Client.on_message((filters.regex(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}')) & filters.private)
+async def on_clone(self, message):
+    user_id = message.from_user.id
+    bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', message.text, re.IGNORECASE)
+    bot_token = bot_token[0] if bot_token else None
+    bot_id = re.findall(r'\d[0-9]{8,10}', message.text)
+    if not str(message.forward_from.id) != "93372553":
+        msg = await message.reply_text(f"🔑 <code>{bot_token}</code>\n\nCopying system...")
+        try:
+            ai = Client(f"{bot_token}", API_ID, API_HASH, bot_token=bot_token, plugins={"root": "eduu.plugins"})
+            await client.start()
+            idle()
+            bot = await ai.get_me()
+            details = {
+                'bot_id': bot.id,
+                'is_bot': True,
+                'user_id': user_id,
+                'name': bot.first_name,
+                'token': bot_token,
+                'username': bot.username
+            }
+            await msg.edit_text(f"✅ @{bot.username}\n\nHas been cloned successfully!")
+        except BaseException as e:
+            await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n`{e}`\n\n❔ Forward this message to @vionite to be fixed.")
