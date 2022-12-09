3dea1287f93a8111aa90be660262ebc4352c604a
Reynaldi Siregar <levinashavila@gmail.com>
2022-12-09 19:30:33 +0700

    [ fix ] clone issues

--- eduu/plugins/clone.py
+++ eduu/plugins/clone.py
@@ -5,20 +5,17 @@ from pyrogram.types import Message
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
 
 from ..config import API_ID, API_HASH
-from ..utils.localization import use_chat_lang
 
 
 @Client.on_message((filters.regex(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}')) & filters.private)
-@use_chat_lang
-async def on_clone(self, message, strings):
+async def on_clone(self, message):
     user_id = message.from_user.id
     bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', message.text, re.IGNORECASE)
     bot_token = bot_token[0] if bot_token else None
     bot_id = re.findall(r'\d[0-9]{8,10}', message.text)
+
     if not str(message.forward_from.id) != "93372553":
-        msg = await message.reply_text(
-            strings("clone_running").format(token=bot_token),
-        )
+        msg = await message.reply_text(f"🔑 <code>{bot_token}</code>\n\nCopying system...")
         try:
             ai = Client(
                 f"{bot_token}", API_ID, API_HASH,
@@ -36,10 +33,6 @@ async def on_clone(self, message, strings):
                 'token': bot_token,
                 'username': bot.username
             }
-            await msg.edit_text(
-                strings("clone_success").format(username=bot.username),
-            )
+            await msg.edit_text(f"✅ @{bot.username}\n\nHas been cloned successfully! don't give the bot token to anyone, because they can control your bot through the third party of Telegram client.")
         except BaseException as e:
-            await msg.edit_text(
-                strings("clone_trouble").format(err=e),
-            )
+            await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{err}</code>\n\n❔ Forward this message to @vionite to be fixed.")
