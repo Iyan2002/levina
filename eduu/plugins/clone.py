de311a0f7bfb799a0d376632b770022efbb4d82f
Reynaldi Siregar <levinashavila@gmail.com>
2022-12-13 20:29:56 +0700

    [ fix ] this more better

--- eduu/plugins/clone.py
+++ eduu/plugins/clone.py
@@ -33,10 +33,6 @@ async def on_clone(self, message):
                 'token': bot_token,
                 'username': bot.username
             }
-            await Client.send_message(
-                chat_id=log_chat,
-                text=(f"{user_name}[<code>{user_id}</code>] cloned @{bot.username}"),
-            )
             await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
         except BaseException as e:
             await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @vionite to be fixed.")
